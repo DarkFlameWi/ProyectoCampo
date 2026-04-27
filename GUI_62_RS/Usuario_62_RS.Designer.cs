a956f8090740770a49dda3b9c520c4a0f196dbc6
hanssgit <HansNoel.Stockl@alumnos.uai.edu.ar>
2026-04-26 21:52:37 -0300

    Update Usuario_62_RS.Designer.cs

--- GUI_62_RS/Usuario_62_RS.Designer.cs
+++ GUI_62_RS/Usuario_62_RS.Designer.cs
@@ -68,9 +68,10 @@
             // RbActivo_62_RS
             // 
             RbActivo_62_RS.AutoSize = true;
-            RbActivo_62_RS.Location = new Point(126, 24);
+            RbActivo_62_RS.Location = new Point(144, 32);
+            RbActivo_62_RS.Margin = new Padding(3, 4, 3, 4);
             RbActivo_62_RS.Name = "RbActivo_62_RS";
-            RbActivo_62_RS.Size = new Size(72, 19);
+            RbActivo_62_RS.Size = new Size(89, 24);
             RbActivo_62_RS.TabIndex = 1;
             RbActivo_62_RS.TabStop = true;
             RbActivo_62_RS.Text = "ACTIVOS";
@@ -79,9 +80,10 @@
             // RbTodo_62_RS
             // 
             RbTodo_62_RS.AutoSize = true;
-            RbTodo_62_RS.Location = new Point(238, 24);
+            RbTodo_62_RS.Location = new Point(272, 32);
+            RbTodo_62_RS.Margin = new Padding(3, 4, 3, 4);
             RbTodo_62_RS.Name = "RbTodo_62_RS";
-            RbTodo_62_RS.Size = new Size(62, 19);
+            RbTodo_62_RS.Size = new Size(78, 24);
             RbTodo_62_RS.TabIndex = 2;
             RbTodo_62_RS.TabStop = true;
             RbTodo_62_RS.Text = "TODOS";
@@ -90,45 +92,47 @@
             // LblUsuario_62_RS
             // 
             LblUsuario_62_RS.AutoSize = true;
-            LblUsuario_62_RS.Location = new Point(23, 26);
+            LblUsuario_62_RS.Location = new Point(26, 35);
             LblUsuario_62_RS.Name = "LblUsuario_62_RS";
-            LblUsuario_62_RS.Size = new Size(62, 15);
+            LblUsuario_62_RS.Size = new Size(79, 20);
             LblUsuario_62_RS.TabIndex = 3;
             LblUsuario_62_RS.Text = "USUARIOS";
             // 
             // LblNumUsu_62_RS
             // 
             LblNumUsu_62_RS.AutoSize = true;
-            LblNumUsu_62_RS.Location = new Point(318, 26);
+            LblNumUsu_62_RS.Location = new Point(363, 35);
             LblNumUsu_62_RS.Name = "LblNumUsu_62_RS";
-            LblNumUsu_62_RS.Size = new Size(118, 15);
+            LblNumUsu_62_RS.Size = new Size(147, 20);
             LblNumUsu_62_RS.TabIndex = 4;
             LblNumUsu_62_RS.Text = "Número de Usuarios:";
             // 
             // LblCantUsu_62_RS
             // 
             LblCantUsu_62_RS.AutoSize = true;
-            LblCantUsu_62_RS.Location = new Point(438, 28);
+            LblCantUsu_62_RS.Location = new Point(501, 37);
             LblCantUsu_62_RS.Name = "LblCantUsu_62_RS";
-            LblCantUsu_62_RS.Size = new Size(38, 15);
+            LblCantUsu_62_RS.Size = new Size(50, 20);
             LblCantUsu_62_RS.TabIndex = 5;
             LblCantUsu_62_RS.Text = "label3";
             // 
             // DgvUsu_62_RS
             // 
             DgvUsu_62_RS.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            DgvUsu_62_RS.Location = new Point(23, 72);
+            DgvUsu_62_RS.Location = new Point(26, 96);
+            DgvUsu_62_RS.Margin = new Padding(3, 4, 3, 4);
             DgvUsu_62_RS.Name = "DgvUsu_62_RS";
             DgvUsu_62_RS.RowHeadersWidth = 51;
-            DgvUsu_62_RS.Size = new Size(453, 150);
+            DgvUsu_62_RS.Size = new Size(518, 200);
             DgvUsu_62_RS.TabIndex = 6;
             DgvUsu_62_RS.CellClick += DgvUsu_62_RS_CellClick;
             // 
             // BtnCrear_62_RS
             // 
-            BtnCrear_62_RS.Location = new Point(501, 25);
+            BtnCrear_62_RS.Location = new Point(573, 33);
+            BtnCrear_62_RS.Margin = new Padding(3, 4, 3, 4);
             BtnCrear_62_RS.Name = "BtnCrear_62_RS";
-            BtnCrear_62_RS.Size = new Size(121, 60);
+            BtnCrear_62_RS.Size = new Size(138, 80);
             BtnCrear_62_RS.TabIndex = 7;
             BtnCrear_62_RS.Text = "Crear";
             BtnCrear_62_RS.UseVisualStyleBackColor = true;
@@ -136,9 +140,10 @@
             // 
             // BtnDesbloquear_62_RS
             // 
-            BtnDesbloquear_62_RS.Location = new Point(501, 91);
+            BtnDesbloquear_62_RS.Location = new Point(573, 121);
+            BtnDesbloquear_62_RS.Margin = new Padding(3, 4, 3, 4);
             BtnDesbloquear_62_RS.Name = "BtnDesbloquear_62_RS";
-            BtnDesbloquear_62_RS.Size = new Size(121, 72);
+            BtnDesbloquear_62_RS.Size = new Size(138, 96);
             BtnDesbloquear_62_RS.TabIndex = 8;
             BtnDesbloquear_62_RS.Text = "Desbloquear";
             BtnDesbloquear_62_RS.UseVisualStyleBackColor = true;
@@ -146,9 +151,10 @@
             // 
             // BtnModificar_62_RS
             // 
-            BtnModificar_62_RS.Location = new Point(501, 169);
+            BtnModificar_62_RS.Location = new Point(573, 225);
+            BtnModificar_62_RS.Margin = new Padding(3, 4, 3, 4);
             BtnModificar_62_RS.Name = "BtnModificar_62_RS";
-            BtnModificar_62_RS.Size = new Size(121, 67);
+            BtnModificar_62_RS.Size = new Size(138, 89);
             BtnModificar_62_RS.TabIndex = 9;
             BtnModificar_62_RS.Text = "Modificar";
             BtnModificar_62_RS.UseVisualStyleBackColor = true;
@@ -156,9 +162,10 @@
             // 
             // BtnActivar_62_RS
             // 
-            BtnActivar_62_RS.Location = new Point(501, 242);
+            BtnActivar_62_RS.Location = new Point(573, 323);
+            BtnActivar_62_RS.Margin = new Padding(3, 4, 3, 4);
             BtnActivar_62_RS.Name = "BtnActivar_62_RS";
-            BtnActivar_62_RS.Size = new Size(121, 67);
+            BtnActivar_62_RS.Size = new Size(138, 89);
             BtnActivar_62_RS.TabIndex = 10;
             BtnActivar_62_RS.Text = "Act. / Desact.";
             BtnActivar_62_RS.UseVisualStyleBackColor = true;
@@ -166,27 +173,30 @@
             // 
             // BtnAplicar_62_RS
             // 
-            BtnAplicar_62_RS.Location = new Point(501, 315);
+            BtnAplicar_62_RS.Location = new Point(573, 420);
+            BtnAplicar_62_RS.Margin = new Padding(3, 4, 3, 4);
             BtnAplicar_62_RS.Name = "BtnAplicar_62_RS";
-            BtnAplicar_62_RS.Size = new Size(121, 66);
+            BtnAplicar_62_RS.Size = new Size(138, 88);
             BtnAplicar_62_RS.TabIndex = 11;
             BtnAplicar_62_RS.Text = "Aplicar";
             BtnAplicar_62_RS.UseVisualStyleBackColor = true;
             // 
             // BtnCancelar_62_RS
             // 
-            BtnCancelar_62_RS.Location = new Point(501, 387);
+            BtnCancelar_62_RS.Location = new Point(573, 516);
+            BtnCancelar_62_RS.Margin = new Padding(3, 4, 3, 4);
             BtnCancelar_62_RS.Name = "BtnCancelar_62_RS";
-            BtnCancelar_62_RS.Size = new Size(121, 61);
+            BtnCancelar_62_RS.Size = new Size(138, 81);
             BtnCancelar_62_RS.TabIndex = 12;
             BtnCancelar_62_RS.Text = "Cancelar";
             BtnCancelar_62_RS.UseVisualStyleBackColor = true;
             // 
             // BtnSalir_62_RS
             // 
-            BtnSalir_62_RS.Location = new Point(501, 453);
+            BtnSalir_62_RS.Location = new Point(573, 604);
+            BtnSalir_62_RS.Margin = new Padding(3, 4, 3, 4);
             BtnSalir_62_RS.Name = "BtnSalir_62_RS";
-            BtnSalir_62_RS.Size = new Size(121, 50);
+            BtnSalir_62_RS.Size = new Size(138, 67);
             BtnSalir_62_RS.TabIndex = 13;
             BtnSalir_62_RS.Text = "Salir";
             BtnSalir_62_RS.UseVisualStyleBackColor = true;
@@ -194,9 +204,11 @@
             // GroupBox
             // 
             GroupBox.Controls.Add(tableLayoutPanel1);
-            GroupBox.Location = new Point(23, 228);
+            GroupBox.Location = new Point(26, 304);
+            GroupBox.Margin = new Padding(3, 4, 3, 4);
             GroupBox.Name = "GroupBox";
-            GroupBox.Size = new Size(277, 261);
+            GroupBox.Padding = new Padding(3, 4, 3, 4);
+            GroupBox.Size = new Size(317, 348);
             GroupBox.TabIndex = 14;
             GroupBox.TabStop = false;
             GroupBox.Text = "DATOS:";
@@ -206,7 +218,7 @@
             tableLayoutPanel1.ColumnCount = 2;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 23F));
             tableLayoutPanel1.Controls.Add(TxtActivo_62_RS, 1, 7);
             tableLayoutPanel1.Controls.Add(TxtBloqueado_62_RS, 1, 6);
             tableLayoutPanel1.Controls.Add(TxtLogIn_62_RS, 1, 5);
@@ -223,7 +235,8 @@
             tableLayoutPanel1.Controls.Add(LblBloqueado_62_RS, 0, 6);
             tableLayoutPanel1.Controls.Add(LblActivo_62_RS, 0, 7);
             tableLayoutPanel1.Controls.Add(txtDni_62_RS, 1, 0);
-            tableLayoutPanel1.Location = new Point(6, 22);
+            tableLayoutPanel1.Location = new Point(7, 29);
+            tableLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 8;
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
@@ -234,56 +247,63 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
             tableLayoutPanel1.RowStyles.Add(new RowStyle());
-            tableLayoutPanel1.Size = new Size(271, 233);
+            tableLayoutPanel1.Size = new Size(310, 293);
             tableLayoutPanel1.TabIndex = 0;
             // 
             // TxtActivo_62_RS
             // 
-            TxtActivo_62_RS.Location = new Point(138, 206);
+            TxtActivo_62_RS.Location = new Point(158, 249);
+            TxtActivo_62_RS.Margin = new Padding(3, 4, 3, 4);
             TxtActivo_62_RS.Name = "TxtActivo_62_RS";
-            TxtActivo_62_RS.Size = new Size(130, 23);
+            TxtActivo_62_RS.Size = new Size(148, 27);
             TxtActivo_62_RS.TabIndex = 15;
             // 
             // TxtBloqueado_62_RS
             // 
-            TxtBloqueado_62_RS.Location = new Point(138, 177);
+            TxtBloqueado_62_RS.Location = new Point(158, 214);
+            TxtBloqueado_62_RS.Margin = new Padding(3, 4, 3, 4);
             TxtBloqueado_62_RS.Name = "TxtBloqueado_62_RS";
-            TxtBloqueado_62_RS.Size = new Size(130, 23);
+            TxtBloqueado_62_RS.Size = new Size(148, 27);
             TxtBloqueado_62_RS.TabIndex = 14;
             // 
             // TxtLogIn_62_RS
             // 
-            TxtLogIn_62_RS.Location = new Point(138, 148);
+            TxtLogIn_62_RS.Location = new Point(158, 179);
+            TxtLogIn_62_RS.Margin = new Padding(3, 4, 3, 4);
             TxtLogIn_62_RS.Name = "TxtLogIn_62_RS";
-            TxtLogIn_62_RS.Size = new Size(130, 23);
+            TxtLogIn_62_RS.Size = new Size(148, 27);
             TxtLogIn_62_RS.TabIndex = 13;
             // 
             // TxtRol_62_RS
             // 
-            TxtRol_62_RS.Location = new Point(138, 119);
+            TxtRol_62_RS.Location = new Point(158, 144);
+            TxtRol_62_RS.Margin = new Padding(3, 4, 3, 4);
             TxtRol_62_RS.Name = "TxtRol_62_RS";
-            TxtRol_62_RS.Size = new Size(130, 23);
+            TxtRol_62_RS.Size = new Size(148, 27);
             TxtRol_62_RS.TabIndex = 12;
             // 
             // TxtEmail_62_RS
             // 
-            TxtEmail_62_RS.Location = new Point(138, 90);
+            TxtEmail_62_RS.Location = new Point(158, 109);
+            TxtEmail_62_RS.Margin = new Padding(3, 4, 3, 4);
             TxtEmail_62_RS.Name = "TxtEmail_62_RS";
-            TxtEmail_62_RS.Size = new Size(130, 23);
+            TxtEmail_62_RS.Size = new Size(148, 27);
             TxtEmail_62_RS.TabIndex = 11;
             // 
             // TxtNombre_62_RS
             // 
-            TxtNombre_62_RS.Location = new Point(138, 61);
+            TxtNombre_62_RS.Location = new Point(158, 74);
+            TxtNombre_62_RS.Margin = new Padding(3, 4, 3, 4);
             TxtNombre_62_RS.Name = "TxtNombre_62_RS";
-            TxtNombre_62_RS.Size = new Size(130, 23);
+            TxtNombre_62_RS.Size = new Size(148, 27);
             TxtNombre_62_RS.TabIndex = 10;
             // 
             // TxtApellido_62_RS
             // 
-            TxtApellido_62_RS.Location = new Point(138, 32);
+            TxtApellido_62_RS.Location = new Point(158, 39);
+            TxtApellido_62_RS.Margin = new Padding(3, 4, 3, 4);
             TxtApellido_62_RS.Name = "TxtApellido_62_RS";
-            TxtApellido_62_RS.Size = new Size(130, 23);
+            TxtApellido_62_RS.Size = new Size(148, 27);
             TxtApellido_62_RS.TabIndex = 9;
             // 
             // LblDni_62_RS
@@ -291,93 +311,95 @@
             LblDni_62_RS.AutoSize = true;
             LblDni_62_RS.Location = new Point(3, 0);
             LblDni_62_RS.Name = "LblDni_62_RS";
-            LblDni_62_RS.Size = new Size(30, 15);
+            LblDni_62_RS.Size = new Size(38, 20);
             LblDni_62_RS.TabIndex = 0;
             LblDni_62_RS.Text = "DNI:";
             // 
             // LblApellido_62_RS
             // 
             LblApellido_62_RS.AutoSize = true;
-            LblApellido_62_RS.Location = new Point(3, 29);
+            LblApellido_62_RS.Location = new Point(3, 35);
             LblApellido_62_RS.Name = "LblApellido_62_RS";
-            LblApellido_62_RS.Size = new Size(59, 15);
+            LblApellido_62_RS.Size = new Size(75, 20);
             LblApellido_62_RS.TabIndex = 1;
             LblApellido_62_RS.Text = "Apellidos:";
             // 
             // LblNombre_62_RS
             // 
             LblNombre_62_RS.AutoSize = true;
-            LblNombre_62_RS.Location = new Point(3, 58);
+            LblNombre_62_RS.Location = new Point(3, 70);
             LblNombre_62_RS.Name = "LblNombre_62_RS";
-            LblNombre_62_RS.Size = new Size(59, 15);
+            LblNombre_62_RS.Size = new Size(73, 20);
             LblNombre_62_RS.TabIndex = 2;
             LblNombre_62_RS.Text = "Nombres:";
             // 
             // LblEmail_62_RS
             // 
             LblEmail_62_RS.AutoSize = true;
-            LblEmail_62_RS.Location = new Point(3, 87);
+            LblEmail_62_RS.Location = new Point(3, 105);
             LblEmail_62_RS.Name = "LblEmail_62_RS";
-            LblEmail_62_RS.Size = new Size(39, 15);
+            LblEmail_62_RS.Size = new Size(49, 20);
             LblEmail_62_RS.TabIndex = 3;
             LblEmail_62_RS.Text = "Email:";
             // 
             // LblRol_62_RS
             // 
             LblRol_62_RS.AutoSize = true;
-            LblRol_62_RS.Location = new Point(3, 116);
+            LblRol_62_RS.Location = new Point(3, 140);
             LblRol_62_RS.Name = "LblRol_62_RS";
-            LblRol_62_RS.Size = new Size(27, 15);
+            LblRol_62_RS.Size = new Size(34, 20);
             LblRol_62_RS.TabIndex = 4;
             LblRol_62_RS.Text = "Rol:";
             // 
             // LblLogIn_62_RS
             // 
             LblLogIn_62_RS.AutoSize = true;
-            LblLogIn_62_RS.Location = new Point(3, 145);
+            LblLogIn_62_RS.Location = new Point(3, 175);
             LblLogIn_62_RS.Name = "LblLogIn_62_RS";
-            LblLogIn_62_RS.Size = new Size(40, 15);
+            LblLogIn_62_RS.Size = new Size(49, 20);
             LblLogIn_62_RS.TabIndex = 5;
             LblLogIn_62_RS.Text = "LogIn:";
             // 
             // LblBloqueado_62_RS
             // 
             LblBloqueado_62_RS.AutoSize = true;
-            LblBloqueado_62_RS.Location = new Point(3, 174);
+            LblBloqueado_62_RS.Location = new Point(3, 210);
             LblBloqueado_62_RS.Name = "LblBloqueado_62_RS";
-            LblBloqueado_62_RS.Size = new Size(67, 15);
+            LblBloqueado_62_RS.Size = new Size(85, 20);
             LblBloqueado_62_RS.TabIndex = 6;
             LblBloqueado_62_RS.Text = "Bloqueado:";
             // 
             // LblActivo_62_RS
             // 
             LblActivo_62_RS.AutoSize = true;
-            LblActivo_62_RS.Location = new Point(3, 203);
+            LblActivo_62_RS.Location = new Point(3, 245);
             LblActivo_62_RS.Name = "LblActivo_62_RS";
-            LblActivo_62_RS.Size = new Size(44, 15);
+            LblActivo_62_RS.Size = new Size(54, 20);
             LblActivo_62_RS.TabIndex = 7;
             LblActivo_62_RS.Text = "Activo:";
             // 
             // txtDni_62_RS
             // 
-            txtDni_62_RS.Location = new Point(138, 3);
+            txtDni_62_RS.Location = new Point(158, 4);
+            txtDni_62_RS.Margin = new Padding(3, 4, 3, 4);
             txtDni_62_RS.Name = "txtDni_62_RS";
-            txtDni_62_RS.Size = new Size(130, 23);
+            txtDni_62_RS.Size = new Size(148, 27);
             txtDni_62_RS.TabIndex = 8;
             // 
             // TxtMensaje_62_RS
             // 
-            TxtMensaje_62_RS.Location = new Point(318, 242);
+            TxtMensaje_62_RS.Location = new Point(363, 323);
+            TxtMensaje_62_RS.Margin = new Padding(3, 4, 3, 4);
             TxtMensaje_62_RS.Multiline = true;
             TxtMensaje_62_RS.Name = "TxtMensaje_62_RS";
-            TxtMensaje_62_RS.Size = new Size(158, 168);
+            TxtMensaje_62_RS.Size = new Size(180, 223);
             TxtMensaje_62_RS.TabIndex = 15;
             // 
             // Usuario_62_RS
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(627, 512);
+            ClientSize = new Size(717, 683);
             Controls.Add(TxtMensaje_62_RS);
             Controls.Add(GroupBox);
             Controls.Add(BtnSalir_62_RS);
@@ -393,6 +415,7 @@
             Controls.Add(LblUsuario_62_RS);
             Controls.Add(RbTodo_62_RS);
             Controls.Add(RbActivo_62_RS);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Usuario_62_RS";
             Text = "Usuario";
             ((System.ComponentModel.ISupportInitialize)DgvUsu_62_RS).EndInit();
